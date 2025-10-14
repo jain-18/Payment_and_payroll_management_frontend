77ec949bfd6467535c3d708a9234e329f71dd9ee
Sudarshan Thiruppathi <sudarshan.thiruppathi@asl.mum.sst>
2025-10-14 19:30:44 +0530

    Updated file structure

---
+++ src/app/organization/org-dashboard-component/org-dashboard-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-org-dashboard-component',
+  imports: [],
+  templateUrl: './org-dashboard-component.html',
+  styleUrl: './org-dashboard-component.css'
+})
+export class OrgDashboardComponent {
+
+}
